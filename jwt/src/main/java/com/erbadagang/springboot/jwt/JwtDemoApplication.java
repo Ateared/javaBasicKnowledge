2ff30d8db29ed280b72eb0528eca22e9b67f15a6
dx-pdemo <pdemo2020@163.com>
2021-01-26 12:41:54 +0800

    jwt1

---
+++ jwt/src/main/java/com/erbadagang/springboot/jwt/JwtDemoApplication.java
@@ -0,0 +1,20 @@
+package com.erbadagang.springboot.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description 应用启动类，演示JWT功能。
+ * @ClassName: JwtDemoApplication
+ * @author: 郭秀志 jbcode@126.com
+ * @date: 2020/7/16 20:25
+ * @Copyright:
+ */
+@SpringBootApplication
+public class JwtDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtDemoApplication.class, args);
+    }
+
+}
